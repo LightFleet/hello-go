33a660f6b2933ab7c86de33d4b6e1a027233bb24
Dmytro Novik <exfriend@live.ru>
2021-10-09 21:31:44 +0300

    Uhm.. A Modification! ..?

--- project.go
+++ project.go
@@ -7,10 +7,36 @@ import (
 )
 
 func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World! %s", time.Now())
+	tryGo()
+	
+
+	fmt.Fprintf(w, "Hella World! %s", time.Now())
 }
 
 func main() {
 	http.HandleFunc("/", greet)
 	http.ListenAndServe(":8080", nil)
+
+
+	tryGo()
+	tryGo()
+	tryGo()
+	tryGo()
+	tryGo()
+}
+
+func tryGo() {
+	var arr[3] int
+	
+	arr[0] = 42
+	arr[1] = 24
+	arr[2] = 01
+	nums := [3]float64 {4.23, 5.23, 98.1}
+
+	fmt.Println(arr[1])
+
+	
+	for i, value := range nums {
+		fmt.Println(value, i)
+	}
 }
